87a5a086ea35f836c7f5d32d19b66ce7b0b58b57
radinmov <radinmovahedi1@gmail.com>
2024-03-09 18:28:35 +0330

    1.0.3

--- src/pages/login/Style.js
+++ src/pages/login/Style.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const  Style =styled.div`
 height:100vh;
   background-image: url('https://img.freepik.com/free-vector/halftone-background-with-circles_23-2148907689.jpg');
-.container_start {
+.container_inner {
     height:500px;
     width:350px;
 }
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,11 +1,20 @@
+import useTitle from "../../componets/Hook";
 import { Style } from "./Style";
 
 export default function Login() {
+    const title = useTitle("Login");
     return (
         <Style>
-            <div className= "container_start d-flex justify-content-center align-items-center  bg-light">
-
+            <div className="container d-flex justify-content-center align-items-center vh-100">
+                <div className="container_inner  bg-light p-4">
+                    <h1>Login page </h1>
+                    <input />
+                    <input />
+                    <h3>Forget Password?</h3>
+                    <button>Login</button>
+                    <h5>Alborz_codeAcademy_Develop</h5>
+                </div>
             </div>
         </Style>
-    )
+    );
 }
